3fd9dff0773b402122490e8d4cf2e0f8a2531e20
F471m4 <40557297+fatima-mo@users.noreply.github.com>
2020-06-29 11:41:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,29 +8,33 @@ let hilow = document.querySelector('.hi-low .hi');
 let low = document.querySelector('.hi-low .low');
 let celsiusConvert = document.querySelector(".celsius-convert");
 let fahrenheitConvert = document.querySelector(".fahrenheit-convert");
+let feelslike = document.querySelector(".FeelsLike p");
+let humidity = document.querySelector(".Humidity p");
+let pressure = document.querySelector(".Pressure p");
+let visibility = document.querySelector(".Visibility p");
 
 const searchbox = document.querySelector('.search-box');
 const weather = {};
 weather.temperature = { unit : "fahrenheit" }
+weather.feelslike = { unit : "fahrenheit" }
 weather.temp_max = { unit : "fahrenheit" }
 weather.temp_min = { unit : "fahrenheit" }
+weather.pressure = { unit :"InHg" }
+weather.visibility = { unit :"mi" }
+
 const api = {
   key: "47ee6d430b2fb1babf79e2e3687b3f79",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 const Rankine = 459;
 
-/*----------------------------------------------------------------------------*/
 
 searchbox.addEventListener('keypress', setQuery);
 function setQuery(evt) {
   if (evt.keyCode == 13) {
     getResults(searchbox.value);
-    console.log(searchbox.value);
-  }
+    console.log(searchbox.value); }
 }
-
-/*----------------------------------------------------------------------------*/
 
 function getResults(query) {
   fetch(`${api.base}weather?q=${query}&appid=${api.key}`)
@@ -48,6 +52,10 @@ function getResults(query) {
         weather.name = data.name;
         weather.temp_max.value = Math.round((data.main.temp_max * (9/5)) - Rankine);
         weather.temp_min.value = Math.round((data.main.temp_min * (9/5)) - Rankine);
+        weather.feelslike.value = Math.round((data.main.feels_like * (9/5)) - Rankine);
+        weather.humidity = data.main.humidity;
+        weather.pressure.value = (data.main.pressure / 33.86388158).toFixed(2); // conversion of Mb to inHg
+        weather.visibility.value = Math.round((data.visibility * (0.62137119224)) / 1000);
       })
       .then(function() {
         displayResults();
@@ -60,11 +68,16 @@ function getResults(query) {
         date.innerText = new Date().toLocaleString('en-us', {  weekday: 'long' });
         weather_el.innerText = weather.description;
         hilow.innerHTML = `${Math.round(weather.temp_min.value)}°F/${Math.round(weather.temp_max.value)}°F`;
+        feelslike.innerHTML = `${Math.round(weather.feelslike.value)}° <span>F</span>`;
+        humidity.innerHTML = `${weather.humidity}<span>%</span>`;
+        pressure.innerHTML = `${weather.pressure.value}<span> inHg</span>`;
+        visibility.innerHTML = `${weather.visibility.value}<span> mi</span>`;
       }
 
       function FahrenheitTocelsius(temperature) {
         return (temperature - 32) / (9/5);
       }
+
       // user-click temperature C
       celsiusConvert.addEventListener("click", function() {
 
@@ -85,6 +98,11 @@ function getResults(query) {
           weather.temp_max.unit = "celsius";
           weather.temp_min.unit = "celsius";
 
+          let celsius4 = FahrenheitTocelsius(weather.feelslike.value);
+          celsius4 = Math.floor(celsius4);
+          feelslike.innerHTML = `${celsius4}° <span>C</span>`;
+          weather.feelslike.unit = "celsius";
+
         }
       });
 
@@ -93,13 +111,16 @@ function getResults(query) {
 
         if(weather.temperature.value === undefined)
           return;
+
         else {
           temperature.innerHTML = `${weather.temperature.value}° <span>F</span>`;
           weather.temperature.unit = "fahrenheit";
           hilow.innerHTML = `${weather.temp_max.value}°F/${weather.temp_min.value}°F`;
           weather.temp_max.unit = "fahrenheit";
           weather.temp_min.unit = "fahrenheit";
+          feelslike.innerHTML = `${weather.feelslike.value}° <span>F</span>`;
+          weather.feelslike.unit = "fahrenheit";
         }
       });
-/*----------------------------------------------------------------------------*/
+  
 }
